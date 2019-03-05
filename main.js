4fb1b89c1f14f2c3fd8299b238f5b2a329ebcb11
Matt Gill <mtgill90@outlook.com>
2019-03-04 19:55:17 -0600

    if else + print to dom

--- main.js
+++ main.js
@@ -1,1 +1,38 @@
-console.log("hi");
+
+const printToDom = (divId, textToPrint) => {
+    const selectedDiv = document.getElementById(divId);
+    selectedDiv.innerHTML += textToPrint;
+};
+
+const drinks = document.getElementById("drinks");
+let action;
+
+let num = prompt("How old are you?");
+let Num = parseInt(num);
+
+const iAmThirsty = (num) => {
+    
+    if (num < 21){
+
+       action = "Drink some water"; 
+       //drinks.innerHTML += action;
+       return action;
+    }
+    else if (num > 20 && num < 65){
+
+        action = "have a beer";
+       // drinks.innerHTML += action;
+        return action;
+    }
+
+    else {
+        action = "take a nap";
+       // drinks.innerHTML += action;
+        return action;
+    }
+};
+
+//iAmThirsty(num);
+
+printToDom("drinks", iAmThirsty(num));
+
